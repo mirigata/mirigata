8bc72559e48bd15f222ef211e1e582c13957543e
Yigal Duppen <yigal@publysher.nl>
2015-09-21 19:31:48 +0200

    Basic navigation flow from homepage to add surprise

--- mirigata/website/urls.py
+++ mirigata/website/urls.py
@@ -3,5 +3,6 @@ from website import views
 
 
 urlpatterns = [
-    url('^$', views.HomepageView.as_view(), name="homepage")
+    url('^$', views.HomepageView.as_view(), name="homepage"),
+    url('^add-surprise$', views.AddSurpriseView.as_view(), name="add-surprise"),
 ]
--- mirigata/website/views.py
+++ mirigata/website/views.py
@@ -2,3 +2,7 @@ from django.views import generic
 
 class HomepageView(generic.TemplateView):
     template_name = "website/index.html"
+
+
+class AddSurpriseView(generic.TemplateView):
+    template_name = "website/add-surprise.html"
