abae242bbcdc3eefcd0ab1ff29f660f89d47db1a
Yigal Duppen <yigal@publysher.nl>
2015-09-21 19:58:19 +0200

    Add absolute URL for Surprises
    This allows the CreateView to work correctly

--- mirigata/surprise/models.py
+++ mirigata/surprise/models.py
@@ -1,6 +1,10 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 
 class Surprise(models.Model):
     link = models.URLField(max_length=500)
     description = models.TextField(max_length=1000)
+
+    def get_absolute_url(self):
+        return reverse('surprise-detail', kwargs={"pk": self.id})
