5b27f45a3b6db420ec88e34f1fe42e0897de07fb
Yigal Duppen <yigal@publysher.nl>
2015-09-22 19:57:37 +0200

    Use Bootstrap3/Crispy form styling

--- mirigata/surprise/forms.py
+++ mirigata/surprise/forms.py
@@ -1,3 +1,6 @@
+from crispy_forms import helper
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.layout import Layout, Field, Submit, Button
 from django import forms
 from . import models
 
@@ -7,3 +10,16 @@ class CreateSurpriseForm(forms.ModelForm):
     class Meta:
         model = models.Surprise
         exclude = ()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = helper.FormHelper()
+        self.helper.layout = Layout(
+            Field('link', css_class="col-sm-12"),
+            Field('description', css_class="col-sm-12", rows=2),
+
+            FormActions(
+                Submit('save', 'Create'),
+                Button('cancel', 'Cancel'),
+            )
+        )
